60abdd628fe70741e1374d20a4600029c0ce0ce6
Miriam <miriammp1997@gmail.com>
2017-05-25 18:19:09 -0500

    add function exito

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function initMap() {
   var laboratorialima = {lat:-12.1191427, lng: -77.0349046};
   var map = new google.maps.Map(document.getElementById("map"),{
-    zoom:20, center: laboratorialima
+    zoom:18, center: laboratorialima
   });
   var markadorLaboratoria = new google.maps.Marker ({
     position: laboratorialima,
@@ -9,6 +9,25 @@ function initMap() {
   });
 }
 
-function map() {
-  
+function buscar() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+  }
 }
+
+var funcionExito = function(posicion) {
+  latitud = posicion.coords.latitude;
+  longitud = posicion.coords.longitude;
+  var miUbicacion = new google.maps.Marker({
+    position: {lat:latitud, lng:longitud},
+    map:map
+  });
+  map.setZoom(18);
+  map.setCenter({lat:latitud, lng:longitud});
+}
+
+var funcionError= function(error){
+  alert("Tenemos un problema con encontrar tu ubicación")
+}
+
+document.getElementById("encuentrame", addEventListener("click", buscar));
