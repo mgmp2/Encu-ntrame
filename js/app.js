17fdece2356b689d1c0f4f039fac40ab221278fd
Miriam <miriammp1997@gmail.com>
2017-05-26 11:51:56 -0500

    ruta lista

--- js/app.js
+++ js/app.js
@@ -26,14 +26,38 @@ function initMap() {
     map.setZoom(18);
     map.setCenter({lat:latitud, lng:longitud});
   }
+  var funcionError= function(error){
+    alert("Tenemos un problema con encontrar tu ubicación")
+  }
+  document.getElementById("encuentrame").addEventListener("click", buscar);
+  var inputPartida = document.getElementById("punto-partida");
+  var inputDestino = document.getElementById("punto-destino");
 
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
 
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+    directionsService.route ({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if(status == "OK") {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert("No encontramos una ruta.");
+      }
+    })
+  }
 
+  directionsDisplay.setMap(map);
 
-var funcionError= function(error){
-  alert("Tenemos un problema con encontrar tu ubicación")
-}
+  var trazarRuta = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
 
+  }
+  document.getElementById("trazar-ruta").addEventListener("click", trazarRuta)
 
-document.getElementById("encuentrame").addEventListener("click", buscar);
 }
